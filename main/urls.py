509cfa64f960ab0dc8ffc4ec17eed08301e3957c
HasibullahAman <hasib.aman7@gmail.com>
2024-09-05 03:46:44 -0400

    done work with views in main

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-
-
-urlpa
-
+urlpatterns = {
+    path('', views.IndexView.as_view(), name='home'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('portfolio/', views.PortfolioView.as_view(), name='portfolios'),
+    path('portfolio/<slug:slug>', views.PortfolioView.as_view(), name= "portfolio"),
+    path('blog', views.BlogView.as_view(), name='blogs'),
+    path('blog/<slug:slug>', views.BlogView.as_view(), name = "blog"),
+} 
