13f92fcaa1d758a8e7ccd1cd78ec75f0877fdb67
HasibullahAman <hasib.aman7@gmail.com>
2025-03-10 22:23:15 +0430

    done-mobail-nav ui

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -47,9 +47,22 @@ const MobileNav = () => {
                     Logo
                 */}
 
-                <div>
-                    Logo
+                <div className="mt-32 mb-4 text-center text-2xl">
+                    <Link href="/">
+                        <h1 className="text-4xl font-semibold">
+                            HUA<span className="text-accent">.</span>
+                        </h1>
+                    </Link>
                 </div>
+                <nav className="flex flex-col justify-center items-center gap-8">
+                    {links.map((link, index) => {
+                        return (
+                            <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
+                                {link.name}
+                            </Link>
+                        )
+                    })}
+                </nav>
             </SheetContent>
         </Sheet>
     )
