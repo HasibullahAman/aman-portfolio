e13d4249e2430014eea1402f3c4154038045504c
HasibullahAman <hasib.aman7@gmail.com>
2025-03-24 00:47:29 +0430

    done counter

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Photo from "../components/Photo";
 import Social from "../components/Social";
 import { Button } from "../components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import Stats from "../components/Stats";
 
 const Home = () => {
   return (
@@ -48,7 +49,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      
+      <Stats />
     </section>
   );
 };
