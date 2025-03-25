65ab0227a92fba3cf8f1f40511c8b334abb9f93f
HasibullahAman <hasib.aman7@gmail.com>
2025-03-25 14:59:54 +0430

    start Services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+import { BSArrowDownRight } from 'react-icons/bs';
+import Link from 'next/link';
+
+const services = [
+    {
+        num: '01',
+        title: 'Web Development',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Maiores ut accusamus laudantium adipisci a aperiam voluptatum et!'
+
+    }
+]
+
+
+
 const Services = () => {
     return (
         <div>
