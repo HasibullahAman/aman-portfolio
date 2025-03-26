f203d3a724dc54a9b10a0d8847ebc7db83ac36d0
HasibullahAman <hasib.aman7@gmail.com>
2025-03-27 01:53:32 +0430

    done resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -19,8 +19,10 @@ import {
 const about = {
     title: "About me",
     description:
-        ` Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus illo
-         voluptatem voluptas doloremque incidunt pariatur?`,
+        ` I am Hasibullah Aman, a Kabul University graduate and web designer skilled in React
+         and Next.js, trained through Meta's Coursera courses. I hold a DeepLearning.ai
+        certificate and have a strong passion for Machine Learning, Deep Learning, and Computer
+         Vision. I am relentless—working tirelessly until I solve my problems.`,
     info: [
         {
             fieldName: "Name",
@@ -220,7 +222,6 @@ const Resume = () => {
                     {/* content */}
                     <div className='min-h-[70vh] w-full'>
 
-
                         {/* experience */}
                         <TabsContent value="experience" className="w-full">
                             <div className="flex flex-col gap-[30px] text-center xl:text-left">
@@ -256,6 +257,7 @@ const Resume = () => {
                                 </ScrollArea>
                             </div>
                         </TabsContent>
+
                         {/* education */}
                         <TabsContent value="education" className="w-full">
                             <div className="flex flex-col gap-[30px] text-center xl:text-left">
@@ -291,13 +293,70 @@ const Resume = () => {
                                 </ScrollArea>
                             </div>
                         </TabsContent>
+
+
+
                         {/* skills */}
-                        <TabsContent value="skills" className="w-full">
-                            Skills
+                        <TabsContent value="skills" className="w-full h-full">
+                            <div className="flex flex-col gap-[30px]">
+                                <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                                    <h3 className='text-4xl font-bold'>{skills.title}</h3>
+                                    <p className='max-w-[600px] text-white/60 mx-auto
+                                    xl:mx-0'>
+                                        {skills.description}
+                                    </p>
+                                </div>
+                                <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+                                xl:gap-[30px] gap-4'>
+                                    {skills.skillList.map((skill, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <TooltipProvider delayDuration={100}>
+                                                    <Tooltip>
+                                                        <TooltipTrigger className='w-full h-[150px]
+                                                        bg-[#232329] rounded-xl flex justify-center
+                                                        items-center group'>
+                                                            <div className='text-6xl group-hover:text-accent
+                                                            transition-all duration-300'>
+                                                                {skill.icon}
+                                                            </div>
+                                                        </TooltipTrigger>
+                                                        <TooltipContent>
+                                                            <p className='capitalize'>
+                                                                {skill.name}
+                                                            </p>
+                                                        </TooltipContent>
+                                                    </Tooltip>
+                                                </TooltipProvider>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
                         </TabsContent>
+
+
+
                         {/* about me */}
-                        <TabsContent value="about" className="w-full">
-                            About me
+                        <TabsContent value="about" className="w-full text-center xl:text-left">
+                            <div className='flex flex-col gap-[30px]'>
+                                <h3 className='text-4xl font-bold'>{about.title}</h3>
+                                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>
+                                    {about.description}
+                                </p>
+                                <ul className='grid grid-cols-1 xl:grid-cols-2 gap-y-6
+                                max-w-[620px] mx-auto xl:mx-0'>
+                                    {about.info.map((item, index) => {
+                                        return (
+                                            <li key={index} className='flex items-center justify-center
+                                            xl:justify-start gap-4'>
+                                                <span className='text-white/60'>{item.fieldName}</span>
+                                                <span className='text-xl'>{item.fieldValue}</span>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
                         </TabsContent>
                     </div>
                 </Tabs>
