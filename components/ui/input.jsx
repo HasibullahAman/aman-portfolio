7a0306177f7f5f366610302aa6abe3a97841c4e9
HasibullahAman <hasib.aman7@gmail.com>
2025-03-29 02:57:53 +0430

    make some changes in input file, by javascriptcode

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -1,21 +1,23 @@
 import * as React from "react";
-
 import { cn } from "../../lib/utils";
 
-const Input = React.forwardRef(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        `flex h-[48px] rounded-md border border-white/10 focus:border-accent
+const Input = React.forwardRef(
+  ({ className, type = "text", ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          `flex h-[48px] rounded-md border border-white/10 focus:border-accent
         font-light bg-primary px-4 py-5 text-base placeholder:text-white/60 outline-none`,
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
 Input.displayName = "Input";
 
 export { Input };
